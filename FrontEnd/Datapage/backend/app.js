b891afd6e4d1c9c95390525a1bfe9a9d2794a52a
DJCooper1104 <dj.cooper1104@gmail.com>
2025-01-13 20:00:04 -0500

    added code to app.js

--- FrontEnd/Datapage/backend/app.js
+++ FrontEnd/Datapage/backend/app.js
@@ -9,9 +9,9 @@ const { engine } = require("express-handlebars");
 
 const app = express();
 app.use(cors());
-app.use('/Homepage', express.static(path.join(__dirname, '../Homepage')));
+app.use("/Homepage", express.static(path.join(__dirname, "../Homepage")));
 app.use(express.static(path.join(__dirname, "../frontend/public")));
-app.use('/images', express.static(path.join(__dirname, '../../images')));
+app.use("/images", express.static(path.join(__dirname, "../../images")));
 
 //Sets our app to use the handlebars engine
 app.set("view engine", "hbs");
@@ -40,6 +40,12 @@ app.get("/api/data", (req, res) => {
       error: "Enter a valid search",
     });
   }
+});
+
+// Route to serve data.html
+app.get("/data", (req, res) => {
+  const dataFilePath = path.join(__dirname, "../frontend/public/data.html");
+  res.sendFile(dataFilePath);
 });
 
 app.get("/api/resources", async (req, res) => {
