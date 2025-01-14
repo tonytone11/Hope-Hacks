8222e552af43f3608ea1063d6b6b8110144d999a
Anthony Montesdeoca <anthony_chen97@yahoo.com>
2025-01-14 11:46:14 -0500

    connecting to data.html and api

--- FrontEnd/Datapage/backend/app.js
+++ FrontEnd/Datapage/backend/app.js
@@ -8,7 +8,13 @@ const fetchData = require("./utils/rapidApi.js");
 const { engine } = require("express-handlebars");
 
 const app = express();
+const corsOptions = {
+  origin: "http://127.0.0.1:5500", // Your Live Server URL
+};
+
+app.use(cors(corsOptions));
 app.use(cors());
+
 app.use("/Homepage", express.static(path.join(__dirname, "../Homepage")));
 app.use(express.static(path.join(__dirname, "../frontend/public")));
 app.use(express.static(path.join(__dirname, "public")));
@@ -16,6 +22,7 @@ app.use("/images", express.static(path.join(__dirname, "../../images")));
 
 //Sets our app to use the handlebars engine
 app.set("view engine", "hbs");
+app.set('views engine', 'html');
 app.set("views", path.join(__dirname, "views"));
 
 app.engine(
@@ -26,6 +33,27 @@ app.engine(
 );
 
 const PORT = 3000;
+
+app.get("/api/resources", async (req, res) => {
+  const { city, state } = req.query; // e.g., "Charlotte, NC"
+  if (!city || !state) {
+    return res.status(400).json({ error: "City and state are required." });
+  }
+  try {
+    const data = await fetchData(city, state);
+    res.status(200).json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch resources." });
+  }
+  // res.send(data);
+});
+
+// Route to serve data.html
+app.get("/data", (req, res) => {
+  const dataFilePath = path.join(__dirname, "../frontend/public/data.html");
+  res.sendFile(dataFilePath);
+});
 
 app.get("/", (req, res) => {
   //Serves the body of the page aka "main.handlebars" to the container //aka "index.handlebars"
@@ -41,28 +69,6 @@ app.get("/api/data", (req, res) => {
       error: "Enter a valid search",
     });
   }
-});
-
-// Route to serve data.html
-app.get("/data", (req, res) => {
-  const dataFilePath = path.join(__dirname, "../frontend/public/data.html");
-  res.sendFile(dataFilePath);
-});
-
-app.get("/api/resources", async (req, res) => {
-  const { city, state } = req.query; // e.g., "Charlotte, NC"
-  if (!city || !state) {
-    return res.status(400).json({ error: "City and state are required." });
-  }
-  const data = await fetchData(city, state);
-  try {
-    const data = await fetchData(city, state);
-    res.status(200).json(data);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to fetch resources." });
-  }
-  // res.send(data);
 });
 
 app.listen(PORT, () => {
--- FrontEnd/Datapage/frontend/public/script.js
+++ FrontEnd/Datapage/frontend/public/script.js
@@ -1,6 +1,6 @@
 // google translate functionality
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({ pageLanguage: 'en', includedLanguages: "ar,zh-CN,en,fr,hi,it,ja,ko,pt,ru,es" }, 'google_translate_element');
+  new google.translate.TranslateElement({ pageLanguage: 'en' }, 'google_translate_element');
 }
 
 document
